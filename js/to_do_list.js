7038050b39f7c6b5e65399d19b760d2ef528b96c
DoloresDai <841856753@qq.com>
2019-01-09 08:32:58 +0800

    Set li class

--- js/to_do_list.js
+++ js/to_do_list.js
@@ -1,8 +1,15 @@
 window.onload = function() {
   leftItems();
+  classLi();
 }
 
 function leftItems() {
   var sum = document.querySelectorAll("li").length;
   document.getElementByClassName("sum").innerHTML = sum;
+}
+
+function classLi() {
+  for (let k = 0; k < sum; k++) {
+    document.querySelectorAll("li")[k].classList.add("list[k]");
+  }
 }
