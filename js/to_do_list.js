1d9c1ea44d4e42f0a6c67249c0d1cdd4bfab90ab
DoloresDai <841856753@qq.com>
2019-01-11 07:00:36 +0800

    Try to code by oop

--- js/to_do_list.js
+++ js/to_do_list.js
@@ -1,41 +1,42 @@
-window.onload = function() {
-  leftItems();
-  classLi();
+class Items {
+  constructor(value, completed, deleted) {
+    this.value = value;
+    this.completed = completed || false;
+    this.deleted = deleted || false;
+  }
+  loadItems(storage) {}
+}
+class Storage {
+  constructor() {
+    this.storage = window.localStorage;
+  }
+  setStorage(items) {
+    this.storage.setItem(`${items}.value`, JSON.stringify(items));
+  }
+  getAll() {
+    let itemsArr = [];
+    for (let i = 0; i < this.storage.length; i++) {
+      let text = this.storage.getItem(this.storage.key(i));
+      let itemsObj = JSON.parse(text);
+      if (itemsObj.value) {
+        let items = new Items(itemsObj.value, itemsObj.completed, itemsObj.deleted)
+        itemsArr.push(itemsObj);
+      }
+    }
+    return itemsArr;
+  }
+  getCompletedItems() {}
 }
 
-function leftItems() {
-  var sum = document.querySelectorAll("li").length;
-  document.getElementByClassName("sum").innerHTML = sum;
+function my$(class_name) {
+  return document.getElementsByClassName(class_name);
 }
 
-function classLi() {
-  for (let k = 0; k < sum; k++) {
-    document.querySelectorAll("li")[k].classList.add("list[k]");
+function enterInput(event) {
+  let value = my$("input").value;
+  if (event.keyCode === 13 && value) {
+    value = "";
   }
 }
-
-function save() {
-  var items = document.querySelector(".items").value;
-  for (let i = 0; i < sum; i++) {
-    window.localStorage.setItem("data" + i, items);
-  }
-}
-
-function readItems() {
-  var value = [];
-  var list = [];
-  for (let j = 0; j < sum; j++) {
-    var keys = window.localStorage.key(j);
-    value[j] = window.localStorage.getItem(keys);
-    var list[j] = document.getElementByClassName(".list[j]");
-    list[j].innerHTML = value[j];
-  }
-}
-
-function deleteItems() {
-  window.localStorage.removeItem(".list[j]");
-}
-
-function clearItems() {
-  window.localStorage.clear();
-}
+let storage = new Storage();
+window.onload = function() {}
